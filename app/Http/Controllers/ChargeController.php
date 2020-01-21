2a603a0a016762477ba45aaa02489475a60e1627
Saeed Sargazi <sa.sargazi@gmail.com>
2020-01-21 19:42:33 +0330

    changes

--- app/Http/Controllers/ChargeController.php
+++ app/Http/Controllers/ChargeController.php
@@ -5,14 +5,12 @@ namespace App\Http\Controllers;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChargeController extends Controller
 {
     public function index()
     {
-        $user = Auth::user();
-
-        $user->createAsStripeCustomer();
 
         return view('stripe');
     }
